243f4f7efae95961b161ad2fa9d4dc99bf5753c2
htspch <htspch@gmail.com>
2015-09-12 09:28:45 +0200

    casperjs in extra dir

---
+++ scrapeUserDetails.js
@@ -0,0 +1,11 @@
+/****
+
+1. get fresh comments from mongodb
+2. write comment/user ids to json file
+3. run casperjs process
+  a) read json
+  b) scrape fb
+  c) write results to json
+4. write json to mongodb
+
+*****/
